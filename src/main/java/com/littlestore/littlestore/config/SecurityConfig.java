d4f9560265cc20ef2202522239b78728c3da0106
eduardo <eduardobcarvalho@outlook.com>
2021-06-07 21:55:13 -0300

    Allow just get in products controller

--- src/main/java/com/littlestore/littlestore/config/SecurityConfig.java
+++ src/main/java/com/littlestore/littlestore/config/SecurityConfig.java
@@ -1,20 +1,37 @@
 package com.littlestore.littlestore.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String[] PUBLIC_MATCHERS = {
-            "/products/**"
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/v1/products/**"
     };
-//
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) {
-//      httpSecurity.
-//    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and().csrf().disable();
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .anyRequest().authenticated();
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
